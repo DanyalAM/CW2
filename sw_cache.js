4c0921ad87b2ca72f6635e5d40fde57d379dcd21
DanyalAM <57116767+DanyalAM@users.noreply.github.com>
2020-01-30 16:35:32 +0000

    Add files via upload

--- sw_cache.js
+++ sw_cache.js
@@ -8,25 +8,25 @@ var cacheName = 'v2';
 // }
 
 var cacheAssets = [
-    '/index.html',
-    '/js/script.js',
-    '/js/component-templates.js',
-    '/image/arrow-down.png',
-    '/image/csIcon.png',
-    '/image/englishIcon.png',
-    '/image/female-avatar.png',
-    '/image/male-avatar.png',
-    '/image/mathsIcon.png',
-    '/image/pencils-yellowbg.jpg',
-    '/image/pianoIcon.png',
-    '/image/loading.jpg',
-    '/image/random.png',
-    '/css/style.css',
-    '/page/login.html',
-    '/page/myAccount.html',
-    '/page/product.html',
-    '/page/register.html',
-    '/server.js',
+    'CW2/index.html',
+    // '/js/script.js',
+    // '/js/component-templates.js',
+    // '/image/arrow-down.png',
+    // '/image/csIcon.png',
+    // '/image/englishIcon.png',
+    // '/image/female-avatar.png',
+    // '/image/male-avatar.png',
+    // '/image/mathsIcon.png',
+    // '/image/pencils-yellowbg.jpg',
+    // '/image/pianoIcon.png',
+    // '/image/loading.jpg',
+    // '/image/random.png',
+    // '/css/style.css',
+    // '/page/login.html',
+    // '/page/myAccount.html',
+    // '/page/product.html',
+    // '/page/register.html',
+    // '/server.js',
 ];
 
 //put our files into the cache
@@ -62,26 +62,26 @@ self.addEventListener('activate', (e) => {
 });
 
 //Call fetch event
-// self.addEventListener('fetch', e => {
-//     console.log('Service Worker: Fetching');
+self.addEventListener('fetch', e => {
+    console.log('Service Worker: Fetching');
 
-//     e.respondWith(fetch(e.request).catch(() => caches.match(e.request)));
-// })
+    e.respondWith(fetch(e.request).catch(() => caches.match(e.request)));
+})
 
 //Call fetch event
 //This fetches the cached content and displayd that instead 
 //is unsecure because of the rest api responses it saves in cache
-self.addEventListener('fetch', function (e) {
-    e.respondWith(
-        caches.match(e.request).then(function (r) {
-            console.log('[Service Worker] Fetching resource: ' + e.request.url);
-            return r || fetch(e.request).then(function (response) {
-                return caches.open(cacheName).then(function (cache) {
-                    console.log('[Service Worker] Caching new resource: ' + e.request.url);
-                    cache.put(e.request, response.clone());
-                    return response;
-                });
-            });
-        })
-    );
-});
+// self.addEventListener('fetch', function (e) {
+//     e.respondWith(
+//         caches.match(e.request).then(function (r) {
+//             console.log('[Service Worker] Fetching resource: ' + e.request.url);
+//             return r || fetch(e.request).then(function (response) {
+//                 return caches.open(cacheName).then(function (cache) {
+//                     console.log('[Service Worker] Caching new resource: ' + e.request.url);
+//                     cache.put(e.request, response.clone());
+//                     return response;
+//                 });
+//             });
+//         })
+//     );
+// });
